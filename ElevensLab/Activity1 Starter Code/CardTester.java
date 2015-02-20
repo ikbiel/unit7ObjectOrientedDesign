d535e38f129fd66fa0b55247ceb341e7db3ac3b8
ikbiel <ikbiel13@gmail.com>
2015-02-20 15:07:16 -0600

    Started Elevens Lab!

--- ElevensLab/Activity1 Starter Code/CardTester.java
+++ ElevensLab/Activity1 Starter Code/CardTester.java
@@ -1,13 +1,22 @@
 /**
  * This is a class that tests the Card class.
  */
-public class CardTester {
+public class CardTester 
+{
 
 	/**
 	 * The main method in this class checks the Card operations for consistency.
 	 *	@param args is not used.
 	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	public static void main(String[] args) 
+	{
+		Card card1 = new Card("4", "Clubs", 4);
+		Card card2 = new Card("4", "Spades", 4);
+		Card card3 = new Card("K", "Hearts", 13);
+		Card card4 = new Card("K", "Hearts", 13);
+		
+		System.out.println(card1.toString() + card2.toString() + card3.toString() + card4.toString());
+		System.out.println(card3.matches(card4));
+		System.out.println(card1.matches(card2));
 	}
 }
